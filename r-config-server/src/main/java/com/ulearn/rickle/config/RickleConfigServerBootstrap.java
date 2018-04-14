a83e948396d58a4cafe5c353bc25382fc943354c
Sunand <spadmanabhan@gainsight.com>
2018-04-14 11:06:33 +0530

    Adding Spring Cloud Config Server to externalize the configuration

---
+++ r-config-server/src/main/java/com/ulearn/rickle/config/RickleConfigServerBootstrap.java
@@ -0,0 +1,18 @@
+package com.ulearn.rickle.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Sunand on 13/04/18.
+ */
+@EnableConfigServer
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class RickleConfigServerBootstrap {
+
+  public static void main(String... args) {
+    SpringApplication.run(RickleConfigServerBootstrap.class, args);
+  }
+}
